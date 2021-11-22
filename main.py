3806e57c066337e1e25890328fae9248a8d6aef3
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-11-22 07:58:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class rekins:
+
+  def __init__(self, klients, veltijums, izmers, materials):
+        
+      self.klients = klients
+      self.veltijums = veltijums
+      self.izmers = izmers
+      self.materials = materials
+
+
+  def izdrukat(self): 
+      pass
+
+  def aprekins(self):
+      pass
+
+

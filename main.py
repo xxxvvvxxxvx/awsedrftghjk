62c92096db10cfa5043982ec9db12453011facf9
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-11-22 08:07:27 +0000

    as

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+
+klients=input("vards uzvards")
+veltijums = input("veltijums")
+izmers = input("platums, garums, augstums")
+materials=input("materiala cena:")
+
 class rekins:
+
 
   def __init__(self, klients, veltijums, izmers, materials):
         
@@ -7,11 +14,15 @@ class rekins:
       self.izmers = izmers
       self.materials = materials
 
+      self.veltijuma_garums=len(self.veltijums)
+      
 
   def izdrukat(self): 
       pass
 
   def aprekins(self):
-      pass
+      darba_samaksa =15
+      PVN= 21
+      
 
 

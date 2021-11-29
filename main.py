521cf6f10a1bb0e198929be3f1330749688de799
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-11-29 08:08:03 +0000

    1

--- main.py
+++ main.py
@@ -20,12 +20,34 @@ class rekins:
       
 
   def izdrukat(self): 
-      pass
+      print("\n\n")
+      print('\002[1m'+"dati"+'\033[0m')
+      print("-"*50)
+      print(f\x1B[3mvards uzvards:\x1B[0m\033[1;32mklients\033[1;0m")
+
 
   def aprekins(self):
       darba_samaksa =15
       PVN= 21
+      produkta_cena = (self.veltijuma_garums) * 1.2 +
+      (self.platums/100 * garums/100 * augstums/100)/ 3 * materiala_cena
+      PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
+      rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
       
+      return rekina_summa
+
+  def sag(self):
+    import csv
+
+    with open (f'{self.klients}.csv', 'w', newline='') as file:
+      writer=csv.writer(file)
+      writer.writerow(['klienta vards','veltijums', 'izmeri', 'kokmateriala cena'])
+
+
+
+
+
+
 
 print(izmers)
 
